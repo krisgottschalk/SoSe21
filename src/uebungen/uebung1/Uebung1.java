21b8c9b7491d38015e1c8ca51a0d68f1816f267c
Christina Gottschalk <christinaisa.g@gmail.com>
2021-04-18 16:39:17 +0200

    Neue Ausgabe eingefuegt

--- src/uebungen/uebung1/Uebung1.java
+++ src/uebungen/uebung1/Uebung1.java
@@ -1,5 +1,11 @@
 package uebungen.uebung1;
 
-public class Uebung1 {
+public class Uebung1 
+{
+	public static void main(String[] args) 
+	{
+		//Zeile 1 etc...
+		System.out.println("Hello You!");
+	}
 
 }
